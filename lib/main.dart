c64a4b09d8463f550649103636a75c1f0d12c582
Sumiran Dahal <sumdahl@pop-os.localdomain>
2022-06-08 17:53:24 +0545

    New change & played with colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int musicNumber) {
+    final player = AudioCache();
+    player.play('note$musicNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.pink),
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-
+}
