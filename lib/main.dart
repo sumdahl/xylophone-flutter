dcf673901702262e30c7ff2af99878533a3b6e77
Sumiran Dahal <sumdahl@pop-os.localdomain>
2022-06-11 13:05:09 +0545

    Important topics of dart functions

--- lib/main.dart
+++ lib/main.dart
@@ -9,55 +9,36 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$musicNumber.wav');
   }
 
+  //2022-06-11 learnt today important topic of functions.
+  Expanded buildKey({Color color, int musicNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: color),
+        onPressed: () {
+          playSound(musicNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            //main axis alignment is vertical & cross axis alignment is horizontal
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.red),
-                onPressed: () {
-                  playSound(1);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.green),
-                onPressed: () {
-                  playSound(2);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.blue),
-                onPressed: () {
-                  playSound(3);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.purple),
-                onPressed: () {
-                  playSound(4);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.orange),
-                onPressed: () {
-                  playSound(5);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.yellow),
-                onPressed: () {
-                  playSound(6);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.pink),
-                onPressed: () {
-                  playSound(7);
-                },
-              ),
+              buildKey(color: Colors.red, musicNumber: 1),
+              buildKey(color: Colors.orange, musicNumber: 2),
+              buildKey(color: Colors.green, musicNumber: 3),
+              buildKey(color: Colors.yellow, musicNumber: 4),
+              buildKey(color: Colors.teal, musicNumber: 5),
+              buildKey(color: Colors.pink, musicNumber: 6),
+              buildKey(color: Colors.blue, musicNumber: 7),
+
             ],
           ),
         ),
